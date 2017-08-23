bf2811e0054098272c7c04fda6414e3fc793dfba
Anne Helby Petersen <ahpe@sund.ku.dk>
2017-08-23 09:45:44 +0200

    First alpha-version up!

---
+++ easyR_v0.1/config/configFile.R
@@ -0,0 +1,8 @@
+initWD <- "P:/Kejsersnitprojekt/easyR/config/"
+
+initData <- list(co2data = "co2data.rdata",
+             irisdata = "iris.rdata")
+
+initModels <- list(`model 1` = "m1.rdata",
+               `model 2` = "model123.rdata",
+               `Cox model` = "coxM.rdata")
